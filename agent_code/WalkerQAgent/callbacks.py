416f2c09d819e28ef758f138ee326a9eadccf624
Yann Ebling <yann.ebling@gmx.de>
2024-08-14 15:07:55 +0200

    Some fixes

--- agent_code/WalkerQAgent/callbacks.py
+++ agent_code/WalkerQAgent/callbacks.py
@@ -1,20 +1,23 @@
 import pickle
 import numpy as np
+import settings as s
 
+NUM_TILES = (s.ROWS - 1)*(s.COLS - 1 - (s.ROWS // 2 - 1)*(s.COLS // 2 - 1))
 
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+
 
 def setup(self):
     # if training is true, a new QWalkerModel is initialized. If training takes too long, there should also be the
     # possibility to further refine a pretrained model in training. All models should be saved  in QWalkerModel.pt
     if self.train:
-        self.logger.info("Setting new model from scratch.")
-        self.model = QWalkerModel()
+        self.logger.info("Setting new model from scratch.", )
+        self.model = QWalkerModel(size_S=NUM_TILES**2)
     # if not training, the current model parameters should be loaded.
     else:
         self.logger.info("Loading model from saved state.")
-        self.model = QWalkerModel(read_from="QWalkerModel.pt")
+        self.model = QWalkerModel(size_S=NUM_TILES**2, read_from="QWalkerModel.pt")
 
 
 def act(self, game_state: dict):
@@ -41,7 +44,9 @@ def state_to_index(game_state: dict):
     complicated. We should definitely look for a better feature extraction for more complex states.
     """
     agent_position = game_state["self"][-1]
-    coin_position = game_state["coins"]
+    print(np.shape(agent_position))
+    coin_position = game_state["coins"][-1]
+    print(np.shape(coin_position))
     arena_height, arena_width = np.shape(game_state["field"])
     assert arena_height == arena_width
     n = arena_height
@@ -50,6 +55,7 @@ def state_to_index(game_state: dict):
     second_digit = convert_pos(coin_position, n)
     index = first_digit * number_of_tiles + second_digit
     return index
+
 
 class QWalkerModel:
     def __init__(self, size_S, size_A=4, read_from=None):
@@ -65,12 +71,12 @@ class QWalkerModel:
         according to the probabilities described by the values at the corresponding entries of the Q matrix.
         """
         index = state_to_index(game_state) # how to do this? Which state is which row in Q?
-        weights = self.Q[:, index]
+        weights = self.Q[:,index]
         # draw a random number to chose an action probabilistically
         random_num = np.random.rand(sum(weights))
         if random_num < weights[0]:
             return ACTIONS[0]
-        for i in range(1,len(weights)):
+        for i in range(1, len(weights)):
             sum_so_far = sum(weights[:i-1])
             if sum_so_far <= random_num < sum_so_far + weights[i]:
                 return ACTIONS[i]
